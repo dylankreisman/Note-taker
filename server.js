654e68eff218e0b625b74cbee5a04d6c13bfbcf0
Dr Dyl <dylkreisman@gmail.com>
2022-07-24 12:59:52 -1000

    made notes.js file and added in route

---
+++ server.js
@@ -0,0 +1,11 @@
+// Need to connect 
+const express = require('express');
+
+// Import our modular routers for /tips and /feedback
+const noteRouter = require('./notes');
+
+const app = express();
+
+app.use('/notes', noteRouter);
+
+module.exports = app;
