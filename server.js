7f00a5131dc156e48d4059f0814767d097fbc9f6
Dr Dyl <dylkreisman@gmail.com>
2022-07-25 12:29:04 -1000

    server found

--- server.js
+++ server.js
@@ -1,23 +1,46 @@
-// Need to connect 
+ 
 const express = require('express');
 const path = require('path')
 const fs = require('fs')
-const noteRouter = require('./js/index.js');
-// Import our modular routers for /tips and /feedback
-//const noteRouter = require('./assets/js/notes.js');
-const PORT = process.env.PORT || 3001;
+const notes = require('./db/db.json');
+const PORT = 3001;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.use('/notes', noteRouter);
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/assets/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+  const note = {
+    id: Math.floor(Math.random() * 1000000),
+    title: req.body.title,
+    text: req.body.text,
+  };
+  notes.push(note);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+  res.json(notes);
+});
 module.exports = app;
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
